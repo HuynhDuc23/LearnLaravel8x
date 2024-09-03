ebf0e8cc8bac740cf5d2aef4116c0aee55d87e44
ducdev <you@example.com>
2024-09-03 21:37:15 +0700

    learn migration

---
+++ supermarket/database/migrations/2024_09_03_141443_create_product_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product', function (Blueprint $table) {
+            //$table->id(); // bigint , autoincrement , primary_key
+
+            $table->increments('id');
+            $table->string('name', 200); // do dai la 200 mac dinh la 255 , ten fields la : name
+            $table->text('description');
+            $table->timestamps(); // auto created_at va updated_at => timestamp
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product');
+    }
+};
